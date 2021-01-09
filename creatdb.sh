37b0ae8c9ae0ec2a2a305400cb5841e9ceab2906
IJNAyanami <lyu891330971@hotmail.com>
2021-01-09 20:19:03 +0800

    Update creatdb.sh

--- creatdb.sh
+++ creatdb.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-DBNAME="typecho"
-LOGIN_CMD="mysql -uroot -plinux1314"
+
+# 接收参数列表 $0 文件名 $1 第一个参数: 用户名 $2 第二个参数: 密码 $3 第三个参数: 数据库名 ...
+USENAME=$1
+
+PASSWORD=$2
+
+DBNAME=$3
+
+LOGIN_CMD="mysql -u${USENAME} -p${PASSWORD}"
+
 echo ${LOGIN_CMD}
+
 create_database() {
 	echo "create database ${DBNAME}"
 	create_db_sql="create database if not exists ${DBNAME} character set utf8mb4"
@@ -14,4 +23,5 @@ create_database() {
 		echo "succeed to create database ${DBNAME}"
 	fi
 }
+
 create_database
